4159e91dce0bc70125ba9d62d9937b7df2a83f6c
Doug <drice@acumensolutions.com>
2018-12-11 11:39:39 -0500

    adding OnClick function to hand function

--- src/app/hand/hand.component.ts
+++ src/app/hand/hand.component.ts
@@ -9,17 +9,33 @@ export class HandComponent implements OnInit {
 
   @Input() Size: string;
 
-  SelectedCards: Array<Number>;
+  //SelectedCards: Array<Number>;
+  SelectedCards: any[] = [];
   Cards = new Array<String>();
+  AvailableVisions = new Array<Number>();
 
-  constructor() { }
+  constructor() {
+    this.SelectedCards.map(e=> false);  
+  }
 
   ngOnInit() {
     var i = 1;
+    for(var x = 0; x < 83; x++) {
+      this.AvailableVisions.push(x);
+    }
+
     do {
-      this.Cards.push('../../assets/visions/cc' + i + '.jpg');
+      var randVision = Math.floor(Math.random() * this.AvailableVisions.length) + 1;
+
+      this.Cards.push('../../assets/visions/cc' + randVision + '.jpg');
+      this.AvailableVisions.splice((randVision - 1), 1);
+
       i++;
     } while(i <= parseInt(this.Size));
+  }
+
+  cardSelected(card) {
+    this.SelectedCards[card] = !this.SelectedCards[card];
   }
 
 }
