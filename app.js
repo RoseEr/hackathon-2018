1f99a19ae44aaf948d23da0dc0fe4d64a023e8c8
Eric Rose <erose@FVFVT5X1HV29.local>
2018-12-12 10:27:54 -0500

    cleaning up server port conflict

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 const http = require('http');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-const WebSocket = require('ws');
  
 var app = express();
 
@@ -28,62 +27,6 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.send(err.status);
-});
-
-const server = http.createServer(app);
-server.listen(process.env.PORT || 3001, () => {
-  console.log(`Server started on port ${server.address().port} :)`);
-});
-
-const wss = new WebSocket.Server({ server });
-
-
-let allPsychics = [];
-let ghost = {};
-
-wss.on('connection', function connection(ws) {
-  console.log('connection made');
-  ws.on('message', function incoming(message) {
-    const messageObject = JSON.parse(message);
-
-    if (messageObject.type === 'new-connection') {
-      var playerId = messageObject.playerId;
-      if (playerId === 'ghost') {
-        ghost = ws;
-        messageObject.psychics.forEach(psychic => {
-          var p = {
-            "ws": {},
-            "id": psychic.id,
-            "person": psychic.person,
-            "place": psychic.place,
-            "thing": psychic.thing
-          }
-
-          allPsychics[psychic.id] = p;
-        });
-      } else {
-        allPsychics[playerId]["ws"] = ws;
-      }
-
-      var response = {
-        "psychics": allPsychics
-      }
-      console.log('response, ', response);
-      ws.send(JSON.stringify(response));
-    }
-
-    if (messageObject.type === 'send-cards') {
-      console.log('Send Card Message Cards: ', messageObject.cards);
-      if (allPsychics[messageObject.player]) {
-        allPsychics[messageObject.player]["ws"].send(JSON.stringify(messageObject));
-      }
-    }
-  });
- 
-  var welcomeMessage = {
-    "type": "welcome"
-  }
-  ws.send(JSON.stringify(welcomeMessage));
 });
 
 module.exports = app;
